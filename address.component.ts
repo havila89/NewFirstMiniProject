1d746a193cb062d249b51a83002d1559a6cf22b4
havila89 <137093522+havila89@users.noreply.github.com>
2023-06-29 12:50:02 -0400

    Submitbutton work

---
+++ address.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-address',
+  templateUrl: './address.component.html',
+  styleUrls: ['./address.component.css'],
+})
+export class AddressComponent implements OnInit {
+  @ViewChild('addressForm') addressForm: NgForm | undefined;
+
+  //initializing variables
+  router = new Router();
+  /*firstname: string = '';
+  lastname: string = '';
+  email: string = '';
+  mstatus = '';
+  location = 'India';
+  spousename = '';*/
+  isSubmit = false;
+  // addressForm: any;
+  address: Address | undefined;
+  constructor() {}
+
+  //navigating to another component
+  onClickSubmit($event: any) {
+    console.log($event);
+    // this.router.navigate(['/personalInfo']);
+    this.isSubmit = true;
+  }
+  onSubmit(addressForm: any) {
+    console.log(addressForm.value.firstname);
+    this.isSubmit = true;
+    if (addressForm.valid) {
+      this.addressForm = addressForm.value;
+    }
+  }
+  ngOnInit() {
+    this.address = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      mstatus: '',
+      spousename: '',
+      location: '',
+    };
+  }
+}
+export class Address {
+  //interface or model
+  firstname: string | undefined;
+  lastname: string | undefined;
+  email: string | undefined;
+  mstatus?: string | undefined;
+  spousename?: string | undefined;
+  location: string | undefined;
+  areacode?: number | undefined;
+}
